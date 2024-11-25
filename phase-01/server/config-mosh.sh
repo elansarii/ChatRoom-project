9bed9b1bf54cc858a010d40116577a64c03f6f35
ansari <elansari1211@gmail.com>
2024-11-25 20:46:24 +0300

    created the config-mosh file

---
+++ phase-01/server/config-mosh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# server/config-mosh.sh
+
+# Install Mosh
+../utils/check-install.sh mosh mosh
+
+# Copy mosh.xml to /etc/firewalld/services/
+cp ../config/mosh.xml /etc/firewalld/services/
+
+# Add Mosh service to firewall
+firewall-cmd --permanent --add-service=mosh
+firewall-cmd --reload
+
+echo "Mosh service installed and firewall configured."
