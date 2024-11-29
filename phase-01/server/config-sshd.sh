169fb10e36b9e49f391304b18b349d6f0296bf23
ansari <elansari1211@gmail.com>
2024-11-29 15:09:34 +0300

    Updated comments in config-sshd file

--- phase-01/server/config-sshd.sh
+++ phase-01/server/config-sshd.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Install OpenSSH server
+# call the check install script to install sshd if it is not installed
 ../utils/check-install.sh sshd openssh-server
-
-# Enable and start sshd service
+# Enable server
 systemctl enable sshd
+# Start server
 systemctl start sshd
+#Show server status
 systemctl status sshd
 
 # Configure sshd_config
@@ -13,16 +14,16 @@ sshd_config="/etc/ssh/sshd_config"
 
 sed -i 's/^#Subsystem sftp .*/Subsystem sftp internal-sftp/' "$sshd_config"
 
-# Restrict SSH access to 'clients' group
+# Check if access to ssh is restricted, if not add restiction
 if ! grep -q '^AllowGroups clients' "$sshd_config"; then
     echo "AllowGroups clients" | tee -a "$sshd_config"
 fi
 
-# Restart sshd service
+# Restart sshd service, to apply changes
 systemctl restart sshd
 
-# Add firewall rules
+# Add firewall rules, and reload to apply changes
 firewall-cmd --permanent --add-service=ssh
 firewall-cmd --reload
 
-echo "SSH server configured to allow access only to 'clients' group."
+echo "SSH server configured. Only clients group memeber can access"
