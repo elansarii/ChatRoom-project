782a6cf1f4b80051210e653b845b020a83290dfe
ansari <elansari1211@gmail.com>
2024-11-29 15:01:33 +0300

    upated comments in generate password

--- phase-01/utils/generate-pass.sh
+++ phase-01/utils/generate-pass.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-# utils/generate-pass.sh
 
 ./check-install.sh pwgen pwgen >/dev/null &2>1
 
-# Generate and print a secure password
+# Generate a 12 char password
 pwgen -s 12 1
