a8b33a849451ca9c98fa334b4f2bdc6bbdd6eaa8
Abdulrahman <abdulrahmanossama72@gmail.com>
2024-11-29 19:17:35 +0300

    Added test-web.sh

--- phase-01/client/test-web.sh
+++ phase-01/client/test-web.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+#Check that curl is installed
+./utils/check-install.sh curl curl
+#Check thhat w3m is installed
+./utils/check-install.sh w3m w3m
+ 
+ SERVER_IP= "serverip"
+
+#Check if web server is accessible ny client
+if curl -I http://$SERVER_IP:80/ 2>dev/null | grep -q "200 OK"; then
+	echo "Web server is accessible. Loading homepage..."
+else 
+	echo "Web server is not accessible. Check configuration."
+	exit 1
+fi
