b1cfd386c6bd59d37deb9a770f352febde439065
ansari <elansari1211@gmail.com>
2024-11-29 14:58:57 +0300

    Update check-install.sh

--- phase-01/utils/check-install.sh
+++ phase-01/utils/check-install.sh
@@ -1,15 +1,22 @@
+<<<<<<< Updated upstream
 #!/bin/bash
 cd "$(dirname "$0")"
 # utils/check-install.sh
 
+=======
+#!/bin/sh
+
+#make sure that there are two arguments passed
+>>>>>>> Stashed changes
 if [ $# -ne 2 ]; then
-    echo "Usage: $0 <command> <package>"
+    echo "Wrong input. Expected input: $0 <command> <package>"
     exit 1
 fi
 
 command="$1"
 package="$2"
 
+#Check if the command is installed, if not install the package
 if ! command -v "$command" >/dev/null 2>&1; then
     echo "Command '$command' not found. Installing package '$package'."
     sudo dnf install -y "$package" >/dev/null
