bab4d75ed067c0c04888cec7270322ff2aeb0994
ansari <elansari1211@gmail.com>
2024-12-03 00:13:46 +0300

    Added foundations of Room class

--- phase-02/code/src/Room.java
+++ phase-02/code/src/Room.java
@@ -1,2 +1,34 @@
+import java.util.*;
+
 public class Room {
+    private String roomName;
+    private Set<User> users = Collections.synchronizedSet(new HashSet<>());
+    private User moderator;
+
+    public Room(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public User getModerator() {
+        return moderator;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void addUser(User user) {
+        synchronized (users) {
+            if (users.isEmpty()) {
+                moderator = user;
+                user.sendMessage("info You are the moderator of this room.");
+            }
+            users.add(user);
+        }
+    }
+
 }
