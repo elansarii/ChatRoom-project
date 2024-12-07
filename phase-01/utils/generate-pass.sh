770ca110ea49571ee8dfe7d3668b2680d1f9cedb
KhaledQarawi <168725253+KhaledQarawi@users.noreply.github.com>
2024-12-07 22:02:22 +0300

    documented generate-pass.sh

--- phase-01/utils/generate-pass.sh
+++ phase-01/utils/generate-pass.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-
+# Check if pwgen is installed, and install it if not
 ./check-install.sh pwgen pwgen >/dev/null &2>1
 
-# Generate a 12 char password
+# Generate a 12 character secure password
 pwgen -s 12 1
